7bce45998bcbfe80d23980b0bd0e34f32bc5af38
seahguan <seahguanlow@gmail.com>
2024-04-05 14:24:11 +0800

    add in readme and execution batch file

---
+++ ChromeExeFile.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\ASUS\Documents\Automation\DerIv-Assessment\
+call robot -d DerIv-Assessment/Results/WEB  -v BROWSER:chrome Tests/WEB/TestScenario1.robot
+call robot -d DerIv-Assessment/Results/API Tests/API/TestScenario3.robot
